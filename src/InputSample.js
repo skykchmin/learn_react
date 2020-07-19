82da0b89ee3c5408539054b54127028348c23eb4
skykchmin <skykchmin@naver.com>
2020-07-19 17:19:40 +0900

    [200719] 29-10. 여러개의 input 상태 관리하기
    [200719] 29-10. 여러개의 input 상태 관리하기

--- src/InputSample.js
+++ src/InputSample.js
@@ -1,21 +1,35 @@
 import React, { useState } from 'react';
 
-function InputSample(){
-    const [text, setText] = useState('');
-    const onChange = (e) => { // 이벤트 객체가 의미하는 것은 만약 이벤트가 수정이 발생했을 때 e 파라미터로 받는다
-        setText(e.target.value); // target이 의미하는것은 e(이벤트)가 발생한 DOM의 정보를 가지고 있다.
-    }
+function InputSample(){ //여러개의 객체를 관리해줘야 한다.
+    const [inputs, setInputs] = useState({
+        name:'',
+        nickname:'',
+    });
 
+    const { name, nickname } = inputs; //비구조화할당을 통해 추출
+    const onChange = (e) => {
+        const { name, value } = e.target;
+
+    setInputs({ //객체를 업데이트할 때는 기존 객체를 복사해야한다.
+        ...inputs, //spread 연산자
+        [name]: value, // 실제 []안에 무엇을 가르키고있는지에 따라 다른 key값이 변경된다 
+    });
+    }
     const onReset = () => {
-        setText('');
+        setInputs({
+            name:'',
+            nickname:'',
+        })
+    
     }
     return(
         <div>
-            <input onChange={onChange} value={text}/>
+            <input name="name" placeholder="이름" onChange={onChange} value={name} />
+            <input name="nickname" placeholder="닉네임" onChange={onChange} value={nickname}/>
             <button onClick={onReset}>초기화</button>
             <div>
                 <b>값: </b>
-                {text}
+               {name} ({nickname})
             </div>
         </div>
     );
